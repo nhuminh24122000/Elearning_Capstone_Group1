28af6958c4c8a135ead3f591c74f05f991153da8
Hung <hung01590159@gmail.com>
2023-06-22 16:07:36 +0700

    push code chung main

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoFooter from '../../assets/img/cyberlogo-white.png'
 
-function footer() {
+// SCSS
+import "./Footer.scss";
+
+export default function Footer() {
   return (
-    <div>footer</div>
-  )
+    <footer>
+      <div className="wrapper pt-5 pb-4">
+        <div className="container">
+          <div className="row g-4">
+            <div className="footer-left col col-lg-4 col-md-4 col-sm-6 col-12">
+              <div className="px-5">
+                <img src={LogoFooter} alt="" />
+                <ul>
+                  <li>
+                    <i class="fa-solid fa-phone"></i>
+                    <span>096.105.1014</span>
+                  </li>
+                  <li>
+                    <i class="fa-solid fa-envelope"></i>
+                    <span>cyberSoft@gmail.com</span>
+                  </li>
+                  <li>
+                  <i class="fa-solid fa-location-dot"></i>
+                  <span>103 Nguyễn Hữu Dật, Hải Châu, ĐN</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col col-lg-4 col-md-4 col-sm-6 col-12">
+              <div className="border-end border-start border-sencondary px-5">
+                <h2>VỀ CYBERSOFT</h2>
+                <ul>
+                  <li>
+                    <NavLink to="/">Courses</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/">About</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/">Contact</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/">Help Desk</NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="footer-newsletter col col-lg-4 col-md-4 col-sm-12 col-12">
+              <h2>NEWSLETTER</h2>
+              <p>
+                Subscribe to our mailing list and get updates to your email inbox.
+              </p>
+              <div class="footer-input">
+                <input
+                  type="email"
+                  name="email"
+                  id=""
+                  placeholder="Email Address"
+                />
+                <i class="fa-solid fa-paper-plane"></i>
+              </div>
+              <ul class="d-flex align-items-center mt-4">
+                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-google"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="rectangle">
+        <div className="text-center pt-3 pb-2">
+          <p>
+            © 2022 Cybersoft All Rights Reserved
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
-
-export default footer
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoHome from '../../assets/img/cyberlogo-black.png'
+import './Header.scss'
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <nav className="navbar navbar-expand-sm">
+        <div className="container d-flex align-items-center">
+          <button
+            className="navbar-toggler d-lg-none"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapsibleNavId"
+            aria-controls="collapsibleNavId"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <NavLink className="navbar-brand" to="/">
+            <img className="LogoHome" src={LogoHome} alt="" />
+          </NavLink>
+          <NavLink className="navbar-listCourse" to={'/coursecategories'}>
+            Khóa Học
+            <i class="fa-solid fa-chevron-down"></i>
+          </NavLink>
+          <div className="d-flex">
+            <div className="d-sm-inline d-none">
+              <div className="d-flex my-2 my-lg-0">
+                <div className="search d-flex">
+                  <button>
+                    <i className="fa fa-search" />
+                  </button>
+                  <input
+                    type="text"
+                    placeholder="Tìm kiếm khóa học, bài viết, ..."
+                  />
+                </div>
+                <NavLink className="btn text-light" to="/search"> Search </NavLink>
+              </div>
+            </div>
+            <div className="d-flex align-items-center">
+							<NavLink className={'header-link'} to={'/signup'}>
+								Sign Up
+							</NavLink>
+              <NavLink className={'header-link'} to={'/signin'}>
+								Sign In
+							</NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
