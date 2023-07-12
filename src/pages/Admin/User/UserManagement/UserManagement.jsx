1c0d1072dec44d99824fd6c283bf21b7b7aa6d6b
dinhhuy1308 <truongdinhhuy1308@gmail.com>
2023-07-12 18:32:22 +0700

    feature search user admin

--- src/pages/Admin/User/UserManagement/UserManagement.jsx
+++ src/pages/Admin/User/UserManagement/UserManagement.jsx
@@ -20,6 +20,12 @@ function UserManagement() {
     const [data, setData] = useState([]);
     const [showPopup, setShowPopup] = useState(false);
 
+    const [key, setKey] = useState('');
+    const [listSearch, setListSearch] = useState([]);
+    const [pageSearch, setPageSearch] = useState(1)
+    const [dataSearch, setDataSearch] = useState([]);
+
+
 
     // Handle User ra giao diện
     const userAdminAPI = async () => {
@@ -45,14 +51,20 @@ function UserManagement() {
     useEffect(() => {
         let newData = listUserAdmin.slice((pageUser - 1) * PAGE_SIZE, pageUser * PAGE_SIZE);
         setData(newData);
-    }, [listUserAdmin, pageUser]);
+
+        let newDataSearch = listSearch.slice((pageSearch - 1) * PAGE_SIZE, pageSearch * PAGE_SIZE)
+        setDataSearch(newDataSearch)
+    }, [listUserAdmin, pageUser, listSearch, pageSearch]);
 
     const handlePageClick = (event) => {
-        setPageUser(event.selected + 1)
+        setPageUser(event.selected + 1) 
+        setPageSearch(event.selected + 1)
     }
 
     const handleUser = () => {
-        return data.map((item, index) => {
+        const list = dataSearch.length > 0 ? dataSearch : data
+
+        return list.map((item, index) => {
             return (
                 <tr key={index}>
                     <td>{((pageUser - 1) * PAGE_SIZE) + index + 1}</td>
@@ -61,7 +73,7 @@ function UserManagement() {
                     <td>{item.email}</td>
                     <td>{item.soDT || item.soDt}</td>
                     <td className='buttons'>
-                        <button className='btn-ghidanh' data-toggle="modal" data-target="#exampleModal" onClick={()=>handlePopupOpen(item.taiKhoan)}>
+                        <button className='btn-ghidanh' data-toggle="modal" data-target="#exampleModal" onClick={() => handlePopupOpen(item.taiKhoan)}>
                             Ghi danh
                         </button>
                         <button className='btn-sua'>
@@ -95,6 +107,7 @@ function UserManagement() {
                 timer: 1500
             })
             userAdminAPI()
+            setListSearch(listSearch.filter(item => item.taiKhoan !== id))
         } catch (err) {
             Swal.fire({
                 position: 'center',
@@ -117,60 +130,98 @@ function UserManagement() {
 
     // Handle khóa học cần xác thực và khóa học đã xác thực ra giao diện
     const courseNotRegister = async (id) => {
-        try{
-            const resp = await axios ({
+        try {
+            const resp = await axios({
                 method: 'post',
                 url: `https://elearningnew.cybersoft.edu.vn/api/QuanLyNguoiDung/LayDanhSachKhoaHocChuaGhiDanh?TaiKhoan=${id}`,
                 headers: {
-					Authorization: `Bearer ${getLocal(ACCESS_TOKEN)}`,
-					TokenCybersoft: `${CYBERSOFT_TOKEN}`,
-				}
+                    Authorization: `Bearer ${getLocal(ACCESS_TOKEN)}`,
+                    TokenCybersoft: `${CYBERSOFT_TOKEN}`,
+                }
             })
             dispatch(setListCourseNotRegister(resp.data))
-        }catch(err) {
+        } catch (err) {
             console.log(err)
         }
     }
 
     const courseNeedAuth = async (id) => {
         try {
-			const resp = await axios({
-				method: 'post',
-				url: 'https://elearningnew.cybersoft.edu.vn/api/QuanLyNguoiDung/LayDanhSachKhoaHocChoXetDuyet',
-				data: {
-					"taiKhoan": id
-				},
-				headers: {
-					Authorization: `Bearer ${getLocal(ACCESS_TOKEN)}`,
-					TokenCybersoft: `${CYBERSOFT_TOKEN}`,
-				}
+            const resp = await axios({
+                method: 'post',
+                url: 'https://elearningnew.cybersoft.edu.vn/api/QuanLyNguoiDung/LayDanhSachKhoaHocChoXetDuyet',
+                data: {
+                    "taiKhoan": id
+                },
+                headers: {
+                    Authorization: `Bearer ${getLocal(ACCESS_TOKEN)}`,
+                    TokenCybersoft: `${CYBERSOFT_TOKEN}`,
+                }
 
-			})
+            })
             dispatch(setListCourseNeedAuth(resp.data))
-		} catch (err) {
-			console.log(err)
-		}
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     const courseCofirm = async (id) => {
         try {
             const resp = await axios({
-				method: 'post',
-				url: 'https://elearningnew.cybersoft.edu.vn/api/QuanLyNguoiDung/LayDanhSachKhoaHocDaXetDuyet',
-				data: {
-					"taiKhoan": id,
-				},
-				headers: {
-					Authorization: `Bearer ${getLocal(ACCESS_TOKEN)}`,
-					TokenCybersoft: `${CYBERSOFT_TOKEN}`,
-				}
+                method: 'post',
+                url: 'https://elearningnew.cybersoft.edu.vn/api/QuanLyNguoiDung/LayDanhSachKhoaHocDaXetDuyet',
+                data: {
+                    "taiKhoan": id,
+                },
+                headers: {
+                    Authorization: `Bearer ${getLocal(ACCESS_TOKEN)}`,
+                    TokenCybersoft: `${CYBERSOFT_TOKEN}`,
+                }
 
-			})
+            })
             dispatch(setListCourseCofirm(resp.data))
         } catch (err) {
             console.log(err)
         }
     }
+
+
+    // Tìm kiếm
+    const handleChangeKey = (e) => {
+        setKey(e.target.value.trim())
+    }
+
+    const handleSearchUser = async (e) => {
+        e.preventDefault();
+
+        try {
+            const resp = await axios({
+                method: 'get',
+                url: `https://elearningnew.cybersoft.edu.vn/api/QuanLyNguoiDung/TimKiemNguoiDung?MaNhom=GP01&tuKhoa=${key}`,
+                headers: {
+                    TokenCybersoft: `${CYBERSOFT_TOKEN}`
+                }
+            })
+            console.log('resp', resp.data)
+            const result = resp.data.filter((item) => {
+                const hoTenMatch = item.hoTen.toLowerCase().includes(key.toLowerCase());
+                const taiKhoanMatch = item.taiKhoan.toLowerCase().includes(key.toLowerCase());
+                return hoTenMatch || taiKhoanMatch
+            })
+            
+            setListSearch(result)
+            setPageSearch(1)
+            setPageUser(1);
+
+
+        } catch (err) {
+            setListSearch([])
+
+            console.log(err)
+        }
+
+    }
+
 
     return (
         <div className='container'>
@@ -178,12 +229,12 @@ function UserManagement() {
                 <h1 >
                     <NavLink to='/admin/useradd'>Thêm Người Dùng </NavLink>
                 </h1>
-                <form className="d-flex my-lg-0">
+                <form className="d-flex my-lg-0" onSubmit={handleSearchUser}>
                     <div className="search d-flex w-100 p-0 pl-4">
                         <button>
                             <i className="fa fa-search" />
                         </button>
-                        <input className='w-100' name="searchText" type="search" placeholder="Nhập vào tài khoản hoặc họ tên người dùng" />
+                        <input onChange={handleChangeKey} className='w-100' name="searchText" type="search" placeholder="Nhập vào tài khoản hoặc họ tên người dùng" />
                     </div>
                     <button className='button-find'>Tìm</button>
                 </form>
@@ -206,9 +257,9 @@ function UserManagement() {
             <div className='py-5'>
                 <Paginate
                     handlePageClick={handlePageClick}
-                    pageCount={Math.ceil(listUserAdmin.length / PAGE_SIZE)}
+                    pageCount={Math.ceil((listSearch.length ? listSearch : listUserAdmin).length / PAGE_SIZE)}
                     forcePage={pageUser - 1}
-                />
+                    />
             </div>
 
             {showPopup && <PopupUser taiKhoan={selectedTaiKhoan} courseNeedAuth={courseNeedAuth} courseCofirm={courseCofirm} />}
